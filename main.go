1f72e8ed96e8765ca48b08bf721517a66f0c3801
laertida <laertida@pm.me>
2025-08-26 02:27:09 -0600

    refactoring Ultrasonic sensor to object & flag use
    This commit introduces a way to handle time in flags

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"lmnl/bestia/lights"
 	"lmnl/bestia/sensors"
 	"os"
-	"periph.io/x/conn/v3/gpio"
-	"periph.io/x/conn/v3/gpio/gpioreg"
 	"periph.io/x/conn/v3/i2c/i2creg"
 	"periph.io/x/host/v3"
 	"sync/atomic"
@@ -16,21 +15,26 @@ import (
 const (
 	TRIG_PORT = "23"
 	ECHO_PORT = "24"
-	LEFT_ADDR = 0x21
+	LEFT_ADDR = 0x22
 	RIGH_ADDR = 0x27
 	IODIR     = 0x00
 	GPIO      = 0x09
 	ADDR      = 0x02
 )
 
-// L 0x40 0x20 0x10 0x08 0x04 0x02
-// R 0x02 0x04 0x08 0x10 0x20 0x40
-
 func main() {
-	// Initialize periph.io
+
+	var timeLow = flag.Int("timeLow", 60, "time in milliseconds where trigger is low.")
+	var timeUp = flag.Int("timeUp", 10, "time in milliseconds where trigger is low.")
+	var timeOut = flag.Int("timeout", 1, "time in seconds to consider timeout for the ultrasonic sensor.")
+	var lightsOn = flag.Bool("ligths", false, "This flags allows to configure if lights will be on or off.")
+
+	// L 0x40 0x20 0x10 0x08 0x04 0x02
+	// R 0x02 0x04 0x08 0x10 0x20 0x40
 	L_MAP := map[int]int{1: 0x40, 2: 0x20, 3: 0x10, 4: 0x08, 5: 0x04, 6: 0x02}
 	R_MAP := map[int]int{1: 0x02, 2: 0x04, 3: 0x08, 4: 0x10, 5: 0x20, 6: 0x40}
 
+	// Initialize periph.io
 	if _, err := host.Init(); err != nil {
 		fmt.Printf("Failed to initialize periph: %v \n", err)
 		os.Exit(1)
@@ -41,30 +45,19 @@ func main() {
 		fmt.Printf("failed to open I2C bus: %v \n", err)
 		os.Exit(1)
 	}
+
 	defer bus.Close()
 
 	left := lights.NewLights("left side", LEFT_ADDR, L_MAP, bus)
 	right := lights.NewLights("right side", RIGH_ADDR, R_MAP, bus)
 	matrix := lights.NewMatrix(left, right)
-	trig := gpioreg.ByName(TRIG_PORT) // GPIO23
-	echo := gpioreg.ByName(ECHO_PORT) // GPIO24
-
-	if trig == nil || echo == nil {
-		fmt.Println("GPIO pins not found")
-		os.Exit(1)
-		return
-	}
-
-	trig.Out(gpio.Low)
-
-	// Channel for distances
-	//distCh := make(chan float64)
+	ultra := sensors.NewUltra(TRIG_PORT, ECHO_PORT, *timeLow, *timeUp, *timeOut)
 
 	var latestDist atomic.Value
 	// Goroutine to read distances periodically
 	go func() {
 		for {
-			dist := sensors.GetDistance(trig, echo)
+			dist := ultra.GetDistance()
 			latestDist.Store(dist)
 			time.Sleep(50 * time.Millisecond)
 		}
@@ -87,10 +80,14 @@ func main() {
 			if dist > 400 {
 				matrix.AllOff()
 			} else if dist < 400 && dist > 100 {
-				matrix.RowStep()
+				if *lightsOn {
+					matrix.RowStep()
+				}
 			} else {
 				interval = time.Duration(100 * time.Millisecond)
-				matrix.RowRandom()
+				if *lightsOn {
+					matrix.RowRandom()
+				}
 			}
 
 			enlapsed := time.Duration(0)
@@ -102,7 +99,6 @@ func main() {
 				newDist, ok := newDistVal.(float64)
 
 				if ok && (dist > 100 && newDist < 100) {
-
 					break
 				}
 			}
--- sensors/Ultra.go
+++ sensors/Ultra.go
@@ -1,40 +1,68 @@
 package sensors
 
 import (
+	"fmt"
+	"os"
 	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
 	"time"
 )
 
-func GetDistance(trig, echo gpio.PinIO) float64 {
-	trig.Out(gpio.Low)
-	time.Sleep(60 * time.Millisecond) // Short delay
+type Ultra struct {
+	trig    gpio.PinIO
+	echo    gpio.PinIO
+	timeLow time.Duration
+	timeUp  time.Duration
+	timeout time.Duration
+}
 
-	trig.Out(gpio.High)
-	time.Sleep(10 * time.Microsecond)
+func NewUltra(trigPort, echoPort string, low, up, tout int) Ultra {
+	trig := gpioreg.ByName(trigPort)
+	echo := gpioreg.ByName(echoPort)
+	if trig == nil || echo == nil {
+		fmt.Println("GPIO pins not found")
+		os.Exit(1)
+	}
+
 	trig.Out(gpio.Low)
+
+	timeLow := time.Duration(low) * time.Millisecond
+	timeUp := time.Duration(up) * time.Millisecond
+	timeout := time.Duration(tout) * time.Second
+
+	return Ultra{trig: trig, echo: echo, timeLow: timeLow, timeUp: timeUp, timeout: timeout}
+}
+
+func (ultra *Ultra) GetDistance() float64 {
+	ultra.trig.Out(gpio.Low)
+	time.Sleep(ultra.timeLow) // Short delay
+
+	ultra.trig.Out(gpio.High)
+	time.Sleep(ultra.timeUp)
+	ultra.trig.Out(gpio.Low)
 
 	// Wait for echo to go HIGH
-	timeout := time.After(1 * time.Second)
+	timeout := time.After(ultra.timeout)
 	var pulseStart time.Time
-	for echo.Read() == gpio.Low {
+	for ultra.echo.Read() == gpio.Low {
 		select {
 		case <-timeout:
 			return -1
 		default:
-			time.Sleep(10 * time.Microsecond)
+			time.Sleep(ultra.timeUp)
 		}
 	}
 	pulseStart = time.Now()
 
 	// Wait for echo to go LOW
-	timeout = time.After(1 * time.Second)
+	timeout = time.After(ultra.timeout)
 	var pulseEnd time.Time
-	for echo.Read() == gpio.High {
+	for ultra.echo.Read() == gpio.High {
 		select {
 		case <-timeout:
 			return -1
 		default:
-			time.Sleep(10 * time.Microsecond)
+			time.Sleep(ultra.timeUp)
 		}
 	}
 	pulseEnd = time.Now()
