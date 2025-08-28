79293981196e2ffcb932a9a824419fb8b9f106be
laertida <laertida@pm.me>
2025-08-28 00:34:25 -0600

    adding test mode

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"lmnl/bestia/sensors"
 	"log/slog"
 	"os"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
 	"periph.io/x/conn/v3/i2c/i2creg"
 	"periph.io/x/host/v3"
 	"sync/atomic"
@@ -17,6 +19,7 @@ import (
 const (
 	TRIG_PORT = "23"
 	ECHO_PORT = "24"
+	TEST_PORT = "17"
 	LEFT_ADDR = 0x23
 	RIGH_ADDR = 0x27
 	IODIR     = 0x00
@@ -37,6 +40,7 @@ func main() {
 	slog.SetDefault(logger)
 
 	slog.Info("Bestia started with this initial values", "timeLow", *timeLow, "timeUp", *timeUp, "timeout", *timeOut, "lightsOn", *lightsOn)
+
 	// L 0x40 0x20 0x10 0x08 0x04 0x02
 	// R 0x02 0x04 0x08 0x10 0x20 0x40
 
@@ -61,6 +65,12 @@ func main() {
 	right := lights.NewLights("right side", RIGH_ADDR, R_MAP, bus, *logger)
 	matrix := lights.NewMatrix(left, right)
 	ultra := sensors.NewUltra(TRIG_PORT, ECHO_PORT, *timeLow, *timeUp, *timeOut, *logger)
+
+	testMode := gpioreg.ByName("17")
+	test := false
+	if testMode.Read() == gpio.High {
+		test = true
+	}
 
 	var latestDist atomic.Value
 	// Goroutine to read distances periodically
@@ -89,19 +99,25 @@ func main() {
 			interval = 1 * time.Second
 
 			if dist > 400 || (dist < 99 && dist > 0) {
-				matrix.AllOff()
-				logger.Info("- off - Lights off due: [dist > 400 || (dist < 99 && dist > 0)]", "dist", dist)
-
+				if test {
+					matrix.AllOn()
+					logger.Info("-  on  - Lights due test mode", "dist", dist)
+				} else {
+					matrix.AllOff()
+					logger.Info("- off - Lights off due: [dist > 400 || (dist < 99 && dist > 0)]", "dist", dist)
+				}
 			} else if dist < 400 && dist > 100 {
-				logger.Info("-normal- Ligths on normal pattern due: [dist < 400 && dist > 100]", "dist", dist)
+
 				if *lightsOn == true {
 					matrix.RowStep()
+					logger.Info("-normal- Ligths on normal pattern due: [dist < 400 && dist > 100]", "dist", dist)
 				}
 			} else {
 				interval = time.Duration(100 * time.Millisecond)
-				logger.Info("-random- Lights on due: dist in rage", "dist", dist)
+
 				if *lightsOn == true {
 					matrix.RowRandom()
+					logger.Info("-random- Lights on due: dist in rage", "dist", dist)
 				}
 			}
 
